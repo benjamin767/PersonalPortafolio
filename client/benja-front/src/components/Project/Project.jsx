155640a82ab8f2bc2fde42dc19ea2319832da76f
Benjamin <natanael.96.01@gmail.com>
2024-08-07 16:19:44 -0300

    improvement in colors and card design

--- client/benja-front/src/components/Project/Project.jsx
+++ client/benja-front/src/components/Project/Project.jsx
@@ -3,14 +3,14 @@ import "./Project.css";
 
 const Project = () => {
     return (<div className="card">
-        <div className="header">
-            <img 
-                src="https://www.godaddy.com/resources/latam/wp-content/uploads/sites/4/pagina-web.jpg?size=3840x0"
-                className="web-img"
-                alt="poyectoWeb"
-            />
-            <h2>Titulo Random de un super proyecto</h2>
-        </div>
+        
+        <img 
+            src="https://www.godaddy.com/resources/latam/wp-content/uploads/sites/4/pagina-web.jpg?size=3840x0"
+            className="web-img"
+            alt="poyectoWeb"
+        />
+        <h2>Titulo Random de un super proyecto</h2>
+        
         <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
             Voluptatem sint totam rerum distinctio adipisci ut dolores autem quis accusantium id incidunt, 
