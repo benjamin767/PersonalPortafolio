45e8b4e02093c222356bc6ac9b86340e686f9b62
Benjamin <natanael.96.01@gmail.com>
2024-09-03 15:56:04 -0300

    added loading component

---
+++ client/benja-front/src/components/Loading/Loading.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Loading.css";
+
+const Loading = () => {
+    return (<>
+        <div class="center-body">
+                <div class="loader-circle-11">
+                <div class="arc"></div>
+                <div class="arc"></div>
+                <div class="arc"></div>
+            </div>
+        </div>
+    </>);
+};
+
+export default Loading;
