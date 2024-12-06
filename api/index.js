5d810a9dbc412fd3515947417b97f6dd7c896cdd
Benjamin <natanael.96.01@gmail.com>
2024-12-06 14:32:21 -0300

    remove get petition

--- api/index.js
+++ api/index.js
@@ -9,32 +9,32 @@ server.listen(port, () => {
         .then(() => {
             console.log(`%s listening at ${port}`);
         });
-    const opciones = {
-        method: 'GET',
-        hostname: 'localhost',
-        port: port,
-        path: path_principal, // Reemplaza '/tu-ruta' con la ruta deseada
-        // headers: {
-        //     'Content-Type': 'application/json'
-        // }
-    };
+    // const opciones = {
+    //     method: 'GET',
+    //     hostname: 'localhost',
+    //     port: port,
+    //     path: path_principal, // Reemplaza '/tu-ruta' con la ruta deseada
+    //     // headers: {
+    //     //     'Content-Type': 'application/json'
+    //     // }
+    // };
         
-    const req = http.request(opciones, (res) => {
-        let datos = '';
-        res.on('data', (chunk) => {
-            datos += chunk;
-        });
-        res.on('end', () => {
-            console.log(datos);
-        });
-    });
+    // const req = http.request(opciones, (res) => {
+    //     let datos = '';
+    //     res.on('data', (chunk) => {
+    //         datos += chunk;
+    //     });
+    //     res.on('end', () => {
+    //         console.log(datos);
+    //     });
+    // });
         
-    req.on('error', (error) => {
-        console.error(error);
-    });
+    // req.on('error', (error) => {
+    //     console.error(error);
+    // });
         
-    //   // Datos a enviar en el cuerpo de la petición
-    // const datosEnviar = JSON.stringify();
-    // req.write(datosEnviar);
-    req.end();
+    // //   // Datos a enviar en el cuerpo de la petición
+    // // const datosEnviar = JSON.stringify();
+    // // req.write(datosEnviar);
+    // req.end();
 });
