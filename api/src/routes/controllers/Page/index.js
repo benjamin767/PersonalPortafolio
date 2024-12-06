504ef971fcd8c3faf2dc3c28c62b904bfefb1e85
Benjamin <natanael.96.01@gmail.com>
2024-12-06 14:29:47 -0300

    updated getPrincipal()

--- api/src/routes/controllers/Page/index.js
+++ api/src/routes/controllers/Page/index.js
@@ -1,26 +1,33 @@
 const { Project, Page } = require("../../../db");
 const jwt = require("jsonwebtoken");
 const { secret, email_page, linkedin, github } = process.env;
+const id = "principal" 
 
 module.exports = {
     getPrincipal: async () => {
-        const [ page ] = await Page.findOrCreate({
-            where: { id: "principal" },
-            default: {
+        const page = await Page.findOrCreate({
+            where: { id },
+            defaults: {
                 name: "Benjamin",
                 surname: "Miño",
-                description: `Desarrollador Web. <br/>
+                description: `Desarrollador Web. 
                     Provengo de la ciudad de Resistencia Chaco, 
-                    pero actualmente vivo en la provincia de Buenos Aires con el objetivo de encontrar la oportunidad laboral en el mundo IT.
-                    Para esto me estuve preparando en un proceso lindo de aprendizaje, 
-                    que aun continua, desarrollo con tecnologías como JavaScript, NodeJs, ReactJs, Reduxjs, PostgresSQL, ExpressJs. 
-                    Incursionar en este mundo me permitió aprender y desarrollarme en nuevos ambientes de trabajo. 
-                    Mi objetivo es trabajar de esto mismo, y ampliar mis conocimientos tecnológicos.`,
+                    pero actualmente vivo en la provincia de Buenos Aires donde obtuve la oportunidad laboral para adentrarme en el mundo IT.
+                    En estos momentos me encuentro trabajando como Soporte IT en FRECOM SRL (con potencial para ser lider en su rubro), 
+                    Tambien desarrollo en tecnologías como JavaScript, NodeJs, ReactJs, Reduxjs, PostgresSQL, ExpressJs. 
+                    Incursionar en este ámbito me permitió aprender y desarrollarme en nuevos ambientes de trabajo. 
+                    Mi objetivo es ganar mas experiencia, y ampliar mis conocimientos tecnológicos.`,
                 github,
                 linkedin,
                 email: email_page
-            }
+            },
+            include: [
+                Project
+            ]
         });
         return page;
-    }
+    },
+    updatedPage: async (name, surname, description, email, github, linkedin) => {
+
+    },
 };
--- api/src/routes/pages.js
+++ api/src/routes/pages.js
@@ -12,4 +12,13 @@ router.get("/principal", async (req, res) => {
     }
 });
 
+router.put("/principal", async (req, res) => {
+    try {
+        const { name, surname, description, email, github, linkedin } = req.body
+        res.status(200).json();
+    } catch(error) {
+        res.status(400).send({ msg: error.message });
+    }
+});
+
 module.exports = router;
